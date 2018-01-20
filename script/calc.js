91ccc84fcdc6329a8b2809134fbca2017d870ccc
Maria Paradeeva <gentlepudding@gmail.com>
2018-01-20 08:20:24 +0500

    Init

---
+++ script/calc.js
@@ -0,0 +1,44 @@
+const $inputs = $('input[type="range"]');
+
+const parameters = {
+    lightsAmount: 0,
+    cornersAmount: 0,
+    pipesAmounts: 0,
+    apartmentsArea: 0,
+};
+
+const summ = $('.summ-block__total');
+
+function calculate() {
+    const price = 
+        parameters.lightsAmount +
+        parameters.cornersAmount +
+        parameters.pipesAmounts +
+        parameters.apartmentsArea;
+
+    summ.text(price);
+}
+
+for (const input of $inputs) {
+    const $input = $(input);
+    const $output = $(input.nextElementSibling);
+    const type = $input.data('calc-parameter-type');
+
+    $input.rangeslider({
+        polyfill: false,
+        onInit: function () {
+            const value = Number($input.attr('value'));
+
+            parameters[type] = value;
+            calculate();
+            $output.text(value);
+        },
+        onSlide: function (position, value) {
+            value = Number(value);
+
+            parameters[type] = value;
+            calculate();
+            $output.text(value);
+        },
+    });
+}
